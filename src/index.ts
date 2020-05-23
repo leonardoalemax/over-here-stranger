c8d8411e0a8ca7d1aa71e50b52bff04f95492fdb
Leonardo Ferreira <leualemax@gmail.com>
2020-05-23 01:56:31 -0300

    add express server

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,19 @@
 
 
+import express from "express";
 import Merchant from "./Merchant";
-// import IsThereAnyDeal , { IGame, IPrice} from "./IsThereAnyDeal"
+
+const app = express();
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(8080, function () {
+    console.log('Example app listening on port 8080!');
+});
+  
 require('dotenv').config();
 
 const server = new Merchant(process.env.BOT_TOKEN || "");
 server.start()
 
-
-
-// const api = new IsThereAnyDeal();
-
-// const start = async () => {
-//     const game = await api.prices("Resident Evil 2");
-//     console.log(`Game: ${game.title}`)
-//     game.prices.forEach((price:IPrice) => {
-//         console.log(`${price.shop.name} - ${price.price_new} (${price.price_old}) ${price.price_cut}%`)
-//     });
-// }
-
-// start();
